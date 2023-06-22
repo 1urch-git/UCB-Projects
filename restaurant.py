00d337e9ee264d5ffd2d6bde0dfb69d11e30b1fb
1urch <charleshreed10@gmail.com>
2023-06-21 22:19:06 -0400

    Create restaurant.py

---
+++ restaurant.py
@@ -0,0 +1,83 @@
+"""
+Scott MacPhee, Boo Park, Chip Reed
+"""
+
+import datetime #print the date time of order
+
+
+def apply_discount(): #calculate discount by multiply percentage by number of tiems
+
+
+
+def time_to_order(): #calculate time to order multiply time by item
+
+
+
+def apply_tax(): #apply tax to total
+
+
+
+def total(): #calculate total by adding and multiplying price by item number
+    
+    
+    apply_tax()
+    return total
+    
+def display_receipt(): #print out the receipt with all ordered items
+
+    border = "*********************************"
+
+    print(border)
+    print("{}, thank you for your order!").format(name) #print out the name of the user
+    print("Item     qty     price") #top row
+    for item in i:
+        print("{}    {}      {}").format()
+
+
+    print(border)
+
+def display_menu(): #print initial menu items
+
+def sandwich(): #hold sandwich variables
+   
+    
+    sandwich_count = int(input("How many sandwiches would you like?"))
+    sandwich_price = 10
+    sandwich_discount = 0.1
+    sandwich_time = 10
+       
+
+def coffee(): 
+
+    coffee_count = int(input("How many coffees would you like?"))
+    coffee_price = 10
+    coffee_discount = 0.1
+    coffee_time = 10
+
+def tea():
+
+    tea_count = int(input("How many teas would you like?"))
+    tea_price = 10
+    tea_discount = 0.1
+    tea_time = 10
+
+
+def salad()
+
+    salad_count = int(input("How many salads would you like?"))
+    salad_price = 10
+    salad_discount = 0.1
+    salad_time = 10
+
+def soup():
+
+    soup_count = int(input("How many soups would you like?"))
+    soup_price = 10
+    soup_discount = 0.1
+    soup_time = 10
+
+def receive_order(): #container to hold functions, ask for order
+
+    display_menu()
+	name = input("Please enter your name.")
+    
